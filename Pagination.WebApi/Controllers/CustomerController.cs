495650f05b136f13143b90cca41eec18e25b26c2
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-19 22:41:40 +0530

    Add project files.

---
+++ Pagination.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlackCoderPaginationWeatherApi.Contexts;
+using BlackCoderPaginationWeatherApi.Filter;
+using BlackCoderPaginationWeatherApi.Helpers;
+using BlackCoderPaginationWeatherApi.Models;
+using BlackCoderPaginationWeatherApi.Services;
+using BlackCoderPaginationWeatherApi.Wrappers;
+
+namespace BlackCoderPaginationWeatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IUriService uriService;
+        public CustomerController(ApplicationDbContext context, IUriService uriService)
+        {
+            this.context = context;
+            this.uriService = uriService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] PaginationFilter filter)
+        {
+            var route = Request.Path.Value;
+            var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+            var pagedData = await context.Customers
+               .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+               .Take(validFilter.PageSize)
+               .ToListAsync();
+            var totalRecords = await context.Customers.CountAsync();
+            var pagedReponse = PaginationHelper.CreatePagedReponse<Customer>(pagedData, validFilter, totalRecords, uriService,route);
+            return Ok(pagedReponse);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var customer = await context.Customers.Where(a => a.Id == id).FirstOrDefaultAsync();
+            return Ok(new Response<Customer>(customer));
+        }
+    }
+}
---
+++ Pagination.WebApi/Services/IUriService.cs
@@ -0,0 +1,13 @@
+﻿using BlackCoderPaginationWeatherApi.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackCoderPaginationWeatherApi.Services
+{
+    public interface IUriService
+    {
+        public Uri GetPageUri(PaginationFilter filter, string route);
+    }
+}
